b7939711241e51feab99190ae527640fc546a38c
Efraim López <46459026+EfraimLA@users.noreply.github.com>
2022-06-17 09:21:14 -0500

    created score calculation method

--- src/main/java/org/acme/prospects/controllers/ProspectController.java
+++ src/main/java/org/acme/prospects/controllers/ProspectController.java
@@ -2,36 +2,77 @@ package org.acme.prospects.controllers;
 
 import org.acme.prospects.models.Prospect;
 import org.acme.prospects.services.ProspectRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/prospects")
 public class ProspectController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProspectController.class);
+
     @Autowired
     ProspectRepository repository;
 
     @GetMapping
-    public List<Prospect> getAll() {
-        return repository.findAll();
+    public ResponseEntity<List<Prospect>> getAll() {
+        return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Prospect> getById(@PathVariable final Long id) {
+        var prospect = repository.findById(id);
+        
+        if (prospect.isPresent()) return new ResponseEntity<>(prospect.get(), HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public Prospect create(Prospect prospect) {
-        return repository.save(prospect);
+    public ResponseEntity<Prospect> create(@RequestBody @Valid Prospect prospect) {
+        return new ResponseEntity<>(repository.save(prospect), HttpStatus.CREATED);
     }
 
-    @PutMapping
-    public Prospect update(Prospect prospect) {
-        return repository.save(prospect);
+    @PutMapping("/{id}")
+    @Modifying
+    public ResponseEntity<Prospect> update(@PathVariable final Long id, @RequestBody @Valid Prospect prospect) {
+        var query = repository.findById(id);
+        if (query.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        var entity = query.get();
+
+        // entity.setName(prospect.getName()); Cannot update this field
+        // entity.setLastName(prospect.getLastName()); Cannot update this field
+        entity.setBirthdate(prospect.getBirthdate());
+        entity.setGender(prospect.getGender());
+        entity.setNationality(prospect.getNationality());
+        entity.setMaritalStatus(prospect.getMaritalStatus());
+        entity.setGrade(prospect.getGrade());
+        entity.setIncome(prospect.getIncome());
+        entity.setIncomeValidation(prospect.getIncomeValidation());
+        entity.setJobAntiquity(prospect.getJobAntiquity());
+        entity.setCreditHistory(prospect.getCreditHistory());
+        entity.setActualCredit(prospect.getActualCredit());
+
+        return new ResponseEntity<>(repository.save(entity), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable final Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<HttpStatus> delete(@PathVariable final Long id) {
+        try {
+            repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- src/main/java/org/acme/prospects/models/Prospect.java
+++ src/main/java/org/acme/prospects/models/Prospect.java
@@ -1,16 +1,30 @@
 package org.acme.prospects.models;
 
-import com.sun.istack.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.text.DecimalFormat;
 import java.util.Date;
 
 @Entity
 @Table(name = "prospects")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Prospect {
+
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final float LOW_RISK = 3.3F;
+    private static final float MEDIUM_RISK = 6.6F;
+    private static final float HIGH_RISK = 10F;
+    private static final int DATE_CONSTANT = (1000 * 60 * 60 * 24 * 365);
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,28 +36,166 @@ public class Prospect {
     @NotNull
     private String lastName;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
+    @NotNull
     private Date birthdate;
 
+    /*
+     * m - male
+     * f - female
+     */
+    @NotNull
     private Character gender;
 
+    /**
+     * m - mexican
+     * f - foreign
+     */
+    @NotNull
     private Character nationality;
 
+    /**
+     * possible values (married, unmarried, single)
+     */
+    @NotNull
     private String maritalStatus;
 
+    @NotNull
+    private Integer economicDependents;
+
+    /**
+     * possible values (prepa, lic, pos)
+     */
+    @NotNull
     private String grade;
 
-    // Financial Data
-    private Integer income;
+    @NotNull
+    private Double income;
 
-    // E stands for Estado de Cuenta & N for Nómina
+    /*
+     * e - Estado de Cuenta
+     * n - Nómina
+     */
+    @NotNull
     private Character incomeValidation;
 
+    @NotNull
     private Integer jobAntiquity;
 
+    @NotNull
     private Boolean creditHistory;
 
+    /*
+     * possible values (tdc, auto, otros)
+     */
+    @NotNull
     private String actualCredit;
 
     @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT)
     private Date createdAt;
+
+    @Transient
+    private Double score;
+
+    public String getScore() {
+        var points = 0.0;
+
+        if (this.getBirthdate() != null) {
+            var age = (new Date().getTime() - this.getBirthdate().getTime()) / DATE_CONSTANT;
+
+            if (age < 25) points += HIGH_RISK;
+            else if (age <= 35) points += MEDIUM_RISK;
+            else if (35 < age) points += LOW_RISK;
+        }
+
+        if (this.getNationality() != null) {
+            switch (this.getNationality()) {
+                case 'f':
+                    points += HIGH_RISK;
+                    break;
+                case 'm':
+                    points += LOW_RISK;
+                    break;
+            }
+        }
+
+        if (this.getMaritalStatus() != null) {
+            switch (this.getMaritalStatus()) {
+                case "single":
+                    points += HIGH_RISK;
+                    break;
+                case "unmarried":
+                    points += MEDIUM_RISK;
+                    break;
+                case "married":
+                    points += LOW_RISK;
+                    break;
+            }
+        }
+
+        if (this.getEconomicDependents() != null) {
+            if (this.getEconomicDependents() == 0) points += LOW_RISK;
+            else if (this.getEconomicDependents() <= 2) points += MEDIUM_RISK;
+            else if (this.getEconomicDependents() > 2) points += HIGH_RISK;
+        }
+
+        if (this.getGrade() != null) {
+            switch (this.getGrade()) {
+                case "prepa":
+                    points += HIGH_RISK;
+                    break;
+                case "lic":
+                    points += MEDIUM_RISK;
+                    break;
+                case "pos":
+                    points += LOW_RISK;
+                    break;
+            }
+        }
+
+        if (this.getIncome() != null) {
+            if (this.getIncome() < 5000) points += HIGH_RISK;
+            else if (this.getIncome() <= 15000) points += MEDIUM_RISK;
+            else if (this.getIncome() > 15000) points += LOW_RISK;
+        }
+
+        if (this.getIncomeValidation() != null) {
+            switch (this.getIncomeValidation()) {
+                case 'e':
+                    points += HIGH_RISK;
+                    break;
+                case 'n':
+                    points += LOW_RISK;
+                    break;
+            }
+        }
+
+        if (this.getJobAntiquity() != null) {
+            if (this.getJobAntiquity() < 2) points += HIGH_RISK;
+            else if (this.getJobAntiquity() <= 5) points += MEDIUM_RISK;
+            else if (this.getJobAntiquity() > 5) points += LOW_RISK;
+        }
+
+        if (this.getCreditHistory() != null) {
+            if (this.getCreditHistory()) points += LOW_RISK;
+            else points += HIGH_RISK;
+        }
+
+        if (this.getActualCredit() != null) {
+            switch (this.getActualCredit()) {
+                case "tdc":
+                    points += MEDIUM_RISK;
+                    break;
+                case "auto":
+                    points += LOW_RISK;
+                    break;
+                case "otros":
+                    points += HIGH_RISK;
+                    break;
+            }
+        }
+
+        return String.format("%.2f", points);
+    }
 }
